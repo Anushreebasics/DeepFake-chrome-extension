57f1804d8cb98f21007af1eedb5b076e50481bee
anushree <aushreebondia@gmail.com>
2024-03-31 13:36:00 +0530

    feat: updated the detection model

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
+import pickle
 from flask import Flask, request, jsonify
 from PIL import Image
 from io import BytesIO
 import base64
 import os
+import cv2
+import numpy as np
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
 
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-UPLOAD_FOLDER = 'DF\\uploads'
+MODEL_PATH = 'model.h5'  
+
+model = load_model(MODEL_PATH)
+model.make_predict_function()
+
+UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/summary', methods=['POST'])
@@ -31,8 +42,32 @@ def process_image():
     image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'process.png')
     pil_image.save(image_path)
 
+    preds=model_predict(image_path,model)
+    # THRESHOLD = 0
+    if preds[0][0] >= 1:
+        result = "Fake"
+    elif preds[0][0] >= 0 :
+        result = "Real"
+    
+    
     # Respond with a success message
-    return jsonify({'message': 'Image processed successfully', 'image_path': image_path}), 200
+    return jsonify({'message': result}), 200
+
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(256, 256))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
